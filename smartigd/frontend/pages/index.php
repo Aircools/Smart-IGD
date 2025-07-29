0fda6dd655c1add1230c07a0798975a81c4ca5de
Aircools <muchtarardhiansyah@gmail.com>
2025-07-29 07:52:18 +0700

    new

--- smartigd/frontend/pages/index.php
+++
@@ -1,9 +0,0 @@
-<?php include '../includes/header.php'; ?>
-<form action="result.php" method="post">
-    <label>Usia: <input type="number" name="age" required></label>
-    <label>Tekanan Darah Sistolik: <input type="number" name="bp_systolic" required></label>
-    <label>GCS: <input type="number" name="gcs" min="3" max="15" required></label>
-    <label><input type="checkbox" name="chest_pain"> Nyeri Dada?</label>
-    <button type="submit">Prediksi</button>
-</form>
-<?php include '../includes/footer.php'; ?>
